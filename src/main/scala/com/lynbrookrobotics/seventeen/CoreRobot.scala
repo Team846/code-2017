082393f6521d2216500241294de939d6ac27b18a
Shadaj Laddad <shadaj@users.noreply.github.com>
2017-01-28 15:02:19 -0800

    Update all libraries for 2017 (#3)

--- src/main/scala/com/lynbrookrobotics/seventeen/CoreRobot.scala
+++ src/main/scala/com/lynbrookrobotics/seventeen/CoreRobot.scala
@@ -4,21 +4,18 @@ import com.lynbrookrobotics.seventeen.config.RobotConfig
 import com.lynbrookrobotics.seventeen.hardware.DriverHardware
 import com.lynbrookrobotics.funkydashboard.FunkyDashboard
 import com.lynbrookrobotics.potassium.clock.Clock
-import com.lynbrookrobotics.potassium.events.EventPolling
-
 import com.lynbrookrobotics.potassium.frc.Implicits._
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
-
+import edu.wpi.first.wpilibj.DriverStation
 import squants.time.Milliseconds
 
 class CoreRobot(implicit config: RobotConfig, clock: Clock) {
   implicit val driverHardware = DriverHardware(config.driver)
 
-  implicit val eventPolling = EventPolling(clock, Milliseconds(20))
+  implicit val eventPolling = DriverStation.getInstance().onDataReceived
 
   implicit val system = ActorSystem("funky-dashboard")
   implicit val materializer = ActorMaterializer()
