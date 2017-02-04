3ca398e3e2da429c85d9f47ddf01a8ba5be4c3d8
dtjong <davintjong@gmail.com>
2017-02-03 20:23:16 -0800

    Add gear collector code (#10)
    * Add gear collector code
    
    * mend

---
+++ src/main/scala/com/lynbrookrobotics/seventeen/gear/collector/GearCollector.scala
@@ -0,0 +1,17 @@
+package com.lynbrookrobotics.seventeen.gear.collector
+
+import com.lynbrookrobotics.potassium.clock.Clock
+import com.lynbrookrobotics.potassium.{Component, PeriodicSignal, Signal}
+import squants.time.Milliseconds
+
+sealed trait GearCollectorState
+case object GearCollectorExtended extends GearCollectorState
+case object GearCollectorRetracted extends GearCollectorState
+
+class GearCollector(implicit hardware: GearCollectorHardware, clock: Clock) extends Component[GearCollectorState](Milliseconds(5)){
+  override def defaultController: PeriodicSignal[GearCollectorState] = Signal.constant(GearCollectorRetracted).toPeriodic
+
+  override def applySignal(signal: GearCollectorState): Unit = {
+    hardware.pneumatic.set(signal == GearCollectorExtended)
+  }
+}
---
+++ src/main/scala/com/lynbrookrobotics/seventeen/gear/collector/GearCollectorConfig.scala
@@ -0,0 +1,3 @@
+package com.lynbrookrobotics.seventeen.gear.collector
+
+case class GearCollectorConfig(port: GearCollectorPorts)
