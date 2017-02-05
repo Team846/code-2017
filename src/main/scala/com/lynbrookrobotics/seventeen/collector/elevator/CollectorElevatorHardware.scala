210ab7e118e7b4b409314d8c78761b00b4d393dd
Shadaj Laddad <shadaj@users.noreply.github.com>
2017-02-04 16:55:39 -0800

    Add component for collector elevator (#16)
    Asana Task: https://app.asana.com/0/219272915179160/261874107688733

---
+++ src/main/scala/com/lynbrookrobotics/seventeen/collector/elevator/CollectorElevatorHardware.scala
@@ -0,0 +1,11 @@
+package com.lynbrookrobotics.seventeen.collector.elevator
+
+import edu.wpi.first.wpilibj.Spark
+
+case class CollectorElevatorHardware(motor: Spark)
+
+object CollectorElevatorHardware{
+  def apply(config: CollectorElevatorConfig): CollectorElevatorHardware ={
+    CollectorElevatorHardware(new Spark(config.port.motor))
+  }
+}
