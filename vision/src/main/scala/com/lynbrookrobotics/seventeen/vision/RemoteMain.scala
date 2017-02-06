a6cc2572a4b23da066125c47ef0ff9baee448028
Sang Hak (Andy) Chun <sanghak@andychun.info>
2017-02-05 16:52:43 -0800

    Stream raw input to dashboard

--- vision/src/main/scala/com/lynbrookrobotics/seventeen/vision/RemoteMain.scala
+++ vision/src/main/scala/com/lynbrookrobotics/seventeen/vision/RemoteMain.scala
@@ -1,18 +1,47 @@
 package com.lynbrookrobotics.seventeen.vision
 
-import akka.actor._
+import scala.collection.mutable.Buffer
+
 import com.lynbrookrobotics.potassium.clock._
 import com.lynbrookrobotics.potassium.remote._
 import com.lynbrookrobotics.potassium.vision._
-import org.opencv.videoio._
+import org.opencv.core._
 
-import squants.Time
-import squants.time.Milliseconds
+import akka.actor._
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import com.lynbrookrobotics.funkydashboard._
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object RemoteMain extends App {
   override def main(args: Array[String]) = {
-    clock.apply(Milliseconds(50))((x: Time) => {
-      println("Time: " + x)
-    })
+    implicit val actorSystem = ActorSystem("host")
+    implicit val materializer = ActorMaterializer()
+
+    val dashboard = Future {
+      val dashboard = new FunkyDashboard
+      Http().bindAndHandle(Route.handlerFlow(dashboard.route), "0.0.0.0", 8080).map { _ =>
+        println("Funky Dashboard is up!")
+        dashboard
+      }
+    }.flatten
+
+    val leftProcessedProvider =
+      new RemoteSignalProvider[Buffer[Rect]]("left-processed", VisionPipeline.leftCameraProcessed)
+    val rightProcessedProvider =
+      new RemoteSignalProvider[Buffer[Rect]]("right-processed", VisionPipeline.rightCameraProcessed)
+
+    dashboard.foreach { board =>
+      board.datasetGroup("Vision").addDataset(new ImageStream("Left Vision Output")(
+        VisionPipeline.leftCamera.map(mat => VisionPipeline.matToBufferedImage(mat)).get
+      ))
+
+      board.datasetGroup("Vision").addDataset(new ImageStream("Right Vision Output")(
+        VisionPipeline.rightCamera.map(mat => VisionPipeline.matToBufferedImage(mat)).get
+      ))
+    }
   }
 }
