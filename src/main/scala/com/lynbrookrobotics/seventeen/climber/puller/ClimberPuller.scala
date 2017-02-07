719eaa284e9462c29696da2e69bf82bebf24eb56
Timothy Yang <wafflemans@users.noreply.github.com>
2017-02-06 18:53:51 -0800

    Changed speed controllers in Climber Puller to CANTalons (#22)
    Defined 2 possible modes for the motors
    
    https://app.asana.com/0/219272915179160/262709399806595

--- src/main/scala/com/lynbrookrobotics/seventeen/climber/puller/ClimberPuller.scala
+++ src/main/scala/com/lynbrookrobotics/seventeen/climber/puller/ClimberPuller.scala
@@ -1,15 +1,32 @@
 package com.lynbrookrobotics.seventeen.climber.puller
 
+import com.ctre.CANTalon
 import com.lynbrookrobotics.potassium.clock.Clock
 import com.lynbrookrobotics.potassium.{Component, PeriodicSignal, Signal}
-import squants.time.Milliseconds
 import squants.{Dimensionless, Percent}
+import squants.time.Milliseconds
+import squants.electro.{ElectricCurrent}
 
-class ClimberPuller(implicit hardware: ClimberPullerHardware, clock: Clock) extends Component[Dimensionless](Milliseconds(5)) {
-  override def defaultController: PeriodicSignal[Dimensionless] = Signal.constant(Percent(0)).toPeriodic
+sealed trait ClimberControlMode
 
-  override def applySignal(signal: Dimensionless): Unit = {
-    hardware.motorA.set(signal.toEach)
-    hardware.motorB.set(signal.toEach)
+case class CurrentMode(current: ElectricCurrent) extends ClimberControlMode
+case class PWMMode(value: Dimensionless) extends ClimberControlMode
+
+class ClimberPuller(implicit hardware: ClimberPullerHardware, clock: Clock) extends Component[ClimberControlMode](Milliseconds(5)) {
+  override def defaultController: PeriodicSignal[ClimberControlMode] = Signal.constant(new PWMMode(Percent(0))).toPeriodic
+
+  override def applySignal(signal: ClimberControlMode): Unit = {
+    signal match {
+      case CurrentMode(current) =>
+        hardware.motorA.changeControlMode(CANTalon.TalonControlMode.Current)
+        hardware.motorB.changeControlMode(CANTalon.TalonControlMode.Current)
+        hardware.motorA.set(current.toAmperes)
+        hardware.motorB.set(current.toAmperes)
+      case PWMMode(value) =>
+        hardware.motorA.changeControlMode(CANTalon.TalonControlMode.PercentVbus)
+        hardware.motorA.changeControlMode(CANTalon.TalonControlMode.PercentVbus)
+        hardware.motorA.set(value.toEach)
+        hardware.motorA.set(value.toEach)
+    }
   }
 }
