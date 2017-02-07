9f932062bf96c14854547790e9d53146e3e94cb4
Shadaj Laddad <shadaj@users.noreply.github.com>
2017-02-06 20:55:37 -0800

    Implement tasks for climbing (#26)

--- src/main/scala/com/lynbrookrobotics/seventeen/climber/extender/ClimberExtender.scala
+++ src/main/scala/com/lynbrookrobotics/seventeen/climber/extender/ClimberExtender.scala
@@ -2,16 +2,24 @@ package com.lynbrookrobotics.seventeen.climber.extender
 
 import com.lynbrookrobotics.potassium.clock.Clock
 import com.lynbrookrobotics.potassium.{Component, PeriodicSignal, Signal}
+import com.lynbrookrobotics.seventeen.collector.extender.{CollectorExtender, CollectorExtenderExtended}
+import com.lynbrookrobotics.seventeen.gear.collector.{GearCollector, GearCollectorExtended}
 import squants.time.Milliseconds
 
 sealed trait ClimberExtenderState
 case object ClimberExtenderExtended extends ClimberExtenderState
 case object ClimberExtenderRetracted extends ClimberExtenderState
 
-class ClimberExtender(implicit hardware: ClimberExtenderHardware, clock: Clock) extends Component[ClimberExtenderState](Milliseconds(5)) {
+class ClimberExtender(implicit hardware: ClimberExtenderHardware,
+                      collectorExtender: CollectorExtender,
+                      gearCollector: GearCollector,
+                      clock: Clock) extends Component[ClimberExtenderState](Milliseconds(5)) {
   override def defaultController: PeriodicSignal[ClimberExtenderState] = Signal.constant(ClimberExtenderRetracted).toPeriodic
 
   override def applySignal(signal: ClimberExtenderState): Unit = {
-    hardware.pneumatic.set(signal == ClimberExtenderExtended)
+    val otherExtended = collectorExtender.peekedController.get.contains(CollectorExtenderExtended) ||
+      gearCollector.peekedController.get.contains(GearCollectorExtended)
+
+    hardware.pneumatic.set(!otherExtended && (signal == ClimberExtenderExtended))
   }
 }
---
+++ src/main/scala/com/lynbrookrobotics/seventeen/climber/puller/ClimberPullerProperties.scala
@@ -0,0 +1,5 @@
+package com.lynbrookrobotics.seventeen.climber.puller
+
+import squants.Dimensionless
+
+case class ClimberPullerProperties(climbSpeed: Dimensionless)
