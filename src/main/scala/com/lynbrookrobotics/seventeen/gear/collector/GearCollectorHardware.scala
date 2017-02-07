4e5c881c666821e7e870bdb2fdfd0d23d7738d06
dtjong <davintjong@gmail.com>
2017-02-06 19:37:32 -0800

    Revert to only one solenoid (#29)

--- src/main/scala/com/lynbrookrobotics/seventeen/gear/collector/GearCollectorHardware.scala
+++ src/main/scala/com/lynbrookrobotics/seventeen/gear/collector/GearCollectorHardware.scala
@@ -2,10 +2,10 @@ package com.lynbrookrobotics.seventeen.gear.collector
 
 import edu.wpi.first.wpilibj.Solenoid
 
-case class GearCollectorHardware(leftCylinder: Solenoid, rightCylinder:Solenoid)
+case class GearCollectorHardware(pneumatic: Solenoid)
 
 object GearCollectorHardware{
   def apply(config: GearCollectorConfig): GearCollectorHardware = {
-    GearCollectorHardware(new Solenoid(config.port.leftCylinder), new Solenoid(config.port.rightCylinder))
+    GearCollectorHardware(new Solenoid(config.port.pneumatic))
   }
 }
