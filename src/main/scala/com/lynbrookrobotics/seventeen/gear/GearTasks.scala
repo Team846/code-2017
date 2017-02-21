c6e852cb052e04632f031dd2ab0325c53a1b1ed5
Timothy Yang <wafflemans@users.noreply.github.com>
2017-02-20 18:29:38 -0800

    Changed collectGearFromGround to use an override button (#45)

--- src/main/scala/com/lynbrookrobotics/seventeen/gear/GearTasks.scala
+++ src/main/scala/com/lynbrookrobotics/seventeen/gear/GearTasks.scala
@@ -2,24 +2,34 @@ package com.lynbrookrobotics.seventeen.gear
 
 import com.lynbrookrobotics.potassium.Signal
 import com.lynbrookrobotics.potassium.clock.Clock
+import com.lynbrookrobotics.potassium.events.ImpulseEvent
 import com.lynbrookrobotics.potassium.frc.ProximitySensor
+import com.lynbrookrobotics.potassium.lighting.{DisplayLighting, LightingComponent}
 import squants.time.Seconds
 import com.lynbrookrobotics.potassium.tasks.{FiniteTask, WaitTask}
+import com.lynbrookrobotics.seventeen.driver.DriverHardware
 import com.lynbrookrobotics.seventeen.gear.tilter.{ExtendTilter, GearTilter, RetractTilter}
 import com.lynbrookrobotics.seventeen.gear.grabber._
+import edu.wpi.first.wpilibj.Joystick
+import com.lynbrookrobotics.potassium.tasks.WaitForImpulseTask
+import com.lynbrookrobotics.potassium.frc.Implicits._
 
 object GearTasks {
-  def loadGearFromGround(implicit tilter: GearTilter,
-                         grabber: GearGrabber, props: Signal[GearGrabberProperties],
-                         hardware: GearGrabberHardware, clock: Clock): FiniteTask = {
+  def loadGearFromGroundAbortable(lightingEffect: Int, buttonOverride: Int, buttonTrigger: Int, lightingComponent: LightingComponent)
+                                 (implicit tilter: GearTilter,
+                                  grabber: GearGrabber,
+                                  props: Signal[GearGrabberProperties],
+                                  hardware: GearGrabberHardware,
+                                  clock: Clock,
+                                  driverHardware: DriverHardware, polling: ImpulseEvent): FiniteTask = {
     val liftGear = new WaitTask(Seconds(0.3)).andUntilDone(
-      new CloseGrabber() and new RetractTilter()
-    )
+      new CloseGrabber()) then new WaitTask(Seconds(0.3)) andUntilDone new RetractTilter()
 
-    val pickUpGear = new OpenGrabberUntilHasGear().andUntilDone(
+    val pickUpGear = new OpenGrabberUntilGearAbortable(buttonOverride).andUntilDone(
       new ExtendTilter()
     )
 
-    pickUpGear then liftGear
+    pickUpGear.then(new WaitForImpulseTask(driverHardware.operatorJoystick.buttonPressed(buttonTrigger).onEnd))
+      .andUntilDone(new DisplayLighting(Signal.constant(lightingEffect), lightingComponent)) then liftGear
   }
 }
---
+++ src/main/scala/com/lynbrookrobotics/seventeen/gear/grabber/OpenGrabberUntilGearAbortable.scala
@@ -0,0 +1,29 @@
+package com.lynbrookrobotics.seventeen.gear.grabber
+
+import com.lynbrookrobotics.potassium.Signal
+import com.lynbrookrobotics.potassium.events.ImpulseEvent
+import com.lynbrookrobotics.potassium.frc.Implicits._
+import com.lynbrookrobotics.potassium.tasks.FiniteTask
+import com.lynbrookrobotics.seventeen.driver.DriverHardware
+
+class OpenGrabberUntilGearAbortable(button: Int)(implicit hardware: GearGrabberHardware, grabber: GearGrabber,
+                                                 props: Signal[GearGrabberProperties], driverHardware: DriverHardware,
+                                                 polling: ImpulseEvent) extends FiniteTask {
+
+  val impulseEvent = driverHardware.operatorJoystick.buttonPressed(button).onStart
+
+  impulseEvent.foreach(() => finished())
+
+  val proximitySensor = hardware.proximitySensor
+  override protected def onStart(): Unit = {
+    grabber.setController(Signal.constant(GearGrabberOpen).toPeriodic.withCheck { _ =>
+      if (proximitySensor.isCloserThan(props.get.detectingDistance)) {
+        finished()
+      }
+    })
+  }
+
+  override protected def onEnd(): Unit = {
+    grabber.resetToDefault()
+  }
+}
