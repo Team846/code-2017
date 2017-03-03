baa2a4999161ec5b1515d3866f7c2e27a8231f46
Sang Hak (Andy) Chun <sanghak@andychun.info>
2017-03-03 07:59:32 -0800

    Add CamSelect to CoreRobot

--- src/main/scala/com/lynbrookrobotics/seventeen/camselect/CamSelectHardware.scala
+++ src/main/scala/com/lynbrookrobotics/seventeen/camselect/CamSelectHardware.scala
@@ -9,12 +9,13 @@ case class CamSelectHardware(
 
 object CamSelectHardware {
   def apply(config: CamSelectConfig): CamSelectHardware = {
-    val url = "http://" + config.properties.coprocessorHostname + config.properties.mjpegPath
+    val uriBase = "http://" + config.properties.coprocessorHostname
+    val uriPath = config.properties.mjpegPath
 
     CamSelectHardware(
-      new HttpCamera("leftCam", url, HttpCamera.HttpCameraKind.kMJPGStreamer),
-      new HttpCamera("rightCam", url, HttpCamera.HttpCameraKind.kMJPGStreamer),
-      new HttpCamera("driverCam", url, HttpCamera.HttpCameraKind.kMJPGStreamer),
+      new HttpCamera("leftCam", uriBase + s":${config.ports.leftCamPort}" + uriPath, HttpCamera.HttpCameraKind.kMJPGStreamer),
+      new HttpCamera("rightCam", uriBase + s":${config.ports.rightCamPort}" + uriPath, HttpCamera.HttpCameraKind.kMJPGStreamer),
+      new HttpCamera("driverCam", uriBase + s":${config.ports.driverCamPort}" + uriPath, HttpCamera.HttpCameraKind.kMJPGStreamer),
       new MjpegServer("serve_Camera Selection", 1182)
     )
   }
