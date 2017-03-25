633185647b3a386c4dd38b7dbff982bf265f63a9
Philip Axelrod <philip.axelrod.web@gmail.com>
2017-03-24 17:18:06 -0700

    Adjust for reduced battery voltage at component level

--- src/main/scala/com/lynbrookrobotics/seventeen/shooter/flywheel/ShooterFlywheel.scala
+++ src/main/scala/com/lynbrookrobotics/seventeen/shooter/flywheel/ShooterFlywheel.scala
@@ -2,17 +2,36 @@ package com.lynbrookrobotics.seventeen.shooter.flywheel
 
 import com.lynbrookrobotics.potassium.clock.Clock
 import com.lynbrookrobotics.potassium.{Component, PeriodicSignal, Signal}
+import com.lynbrookrobotics.seventeen.driver.DriverHardware
 import squants.time.Milliseconds
 import squants.{Dimensionless, Percent}
 
-class ShooterFlywheel(implicit hardware: ShooterFlywheelHardware, clock: Clock)
+
+class ShooterFlywheel(implicit hardware: ShooterFlywheelHardware, clock: Clock, driverHardware: DriverHardware)
   extends Component[DoubleFlywheelSignal](Milliseconds(5)) {
+
+  val NominalVoltage = 13.0
+
   override def defaultController = Signal.constant(
     DoubleFlywheelSignal(Percent(0), Percent(0))
   ).toPeriodic
 
+  /**
+    * Compensate for reduced battery voltage
+    * @return factor to multiply inputs to imitate behaviour of system where battery voltage
+    *         is nominal
+    */
+  def voltageFactor: Double = {
+    val batteryVoltage = driverHardware.station.getBatteryVoltage
+    if (batteryVoltage > NominalVoltage / 2) { // reasonable measurement must be than 6 volts
+      NominalVoltage / driverHardware.station.getBatteryVoltage
+    } else {
+      NominalVoltage
+    }
+  }
+
   override def applySignal(signal: DoubleFlywheelSignal): Unit = {
-    hardware.leftMotor.set(signal.left.toEach)
-    hardware.rightMotor.set(signal.right.toEach)
+    hardware.leftMotor.set(voltageFactor * signal.left.toEach)
+    hardware.rightMotor.set(voltageFactor * signal.right.toEach)
   }
 }
