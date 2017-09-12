861d5c8f65805d35f79109d0dccc5c16c0b84d0f
Kunal Sheth <kunal@kunalsheth.info>
2017-09-11 18:15:03 -0700

    Added gear grabber / tilter safety.

--- src/main/scala/com/lynbrookrobotics/seventeen/gear/grabber/GearGrabber.scala
+++ src/main/scala/com/lynbrookrobotics/seventeen/gear/grabber/GearGrabber.scala
@@ -1,9 +1,9 @@
 package com.lynbrookrobotics.seventeen.gear.grabber
 
-import com.lynbrookrobotics.potassium.clock.Clock
-import com.lynbrookrobotics.potassium.{Component, PeriodicSignal, Signal}
 import com.lynbrookrobotics.potassium.streams.Stream
-import squants.time.{Milliseconds, Seconds}
+import com.lynbrookrobotics.potassium.{Component, Signal}
+import com.lynbrookrobotics.seventeen.gear.tilter.GearTilter
+import squants.time.Milliseconds
 
 sealed trait GearGrabberState
 
@@ -11,10 +11,29 @@ case object GearGrabberOpen extends GearGrabberState
 
 case object GearGrabberClosed extends GearGrabberState
 
-class GearGrabber(val coreTicks: Stream[Unit])(implicit hardware: GearGrabberHardware) extends Component[GearGrabberState](Milliseconds(5)) {
+class GearGrabber(val coreTicks: Stream[Unit])(implicit hardware: GearGrabberHardware, gearTilterF: () => Option[GearTilter])
+  extends Component[GearGrabberState](Milliseconds(5)) {
+
+  lazy val gearTilter = gearTilterF()
+
   override def defaultController: Stream[GearGrabberState] = coreTicks.mapToConstant(GearGrabberClosed)
 
+  private var curLastOpenedTime: Long = 0
+  val lastOpenTime = Signal(curLastOpenedTime)
+
+  lazy val gearExtended = gearTilter.map(_.lastExtendTime
+    .map(t => System.currentTimeMillis() - t <= 1000))
+    .getOrElse(Signal.constant(false))
+
   override def applySignal(signal: GearGrabberState): Unit = {
-    hardware.pneumatic.set(signal == GearGrabberOpen)
+    if (!gearExtended.get) {
+      hardware.pneumatic.set(false)
+    } else {
+      hardware.pneumatic.set(signal == GearGrabberOpen)
+
+      if (signal == GearGrabberOpen) {
+        curLastOpenedTime = System.currentTimeMillis()
+      }
+    }
   }
 }
--- src/main/scala/com/lynbrookrobotics/seventeen/gear/tilter/GearTilter.scala
+++ src/main/scala/com/lynbrookrobotics/seventeen/gear/tilter/GearTilter.scala
@@ -1,10 +1,10 @@
 package com.lynbrookrobotics.seventeen.gear.tilter
 
-import com.lynbrookrobotics.potassium.clock.Clock
 import com.lynbrookrobotics.potassium.streams.Stream
-import com.lynbrookrobotics.potassium.{Component, PeriodicSignal, Signal}
+import com.lynbrookrobotics.potassium.{Component, Signal}
 import com.lynbrookrobotics.seventeen.collector.extender.CollectorExtender
-import squants.time.{Milliseconds, Seconds}
+import com.lynbrookrobotics.seventeen.gear.grabber.GearGrabber
+import squants.time.Milliseconds
 
 sealed trait GearTilterState
 
@@ -13,9 +13,10 @@ case object GearTilterExtended extends GearTilterState
 case object GearTilterRetracted extends GearTilterState
 
 class GearTilter(val coreTicks: Stream[Unit])(implicit hardware: GearTilterHardware,
-                 collectorExtenderF: () => Option[CollectorExtender]) extends Component[GearTilterState](Milliseconds(5)) {
+                                              collectorExtenderF: () => Option[CollectorExtender], gearGrabberF: () => Option[GearGrabber]) extends Component[GearTilterState](Milliseconds(5)) {
   override def defaultController: Stream[GearTilterState] = coreTicks.mapToConstant(GearTilterRetracted)
 
+  lazy val gearGrabber = gearGrabberF()
   lazy val collectorExtender = collectorExtenderF()
 
   private var curLastExtendTime: Long = 0
@@ -25,8 +26,17 @@ class GearTilter(val coreTicks: Stream[Unit])(implicit hardware: GearTilterHardw
     map(t => System.currentTimeMillis() - t <= 1000)).
     getOrElse(Signal.constant(false))
 
+  private var curLastOpenTime: Long = 0
+  val lastOpenTime = Signal(curLastOpenTime)
+
+  lazy val gearOpened = gearGrabber.map(_.lastOpenTime.
+    map(t => System.currentTimeMillis() - t <= 1000)).
+    getOrElse(Signal.constant(false))
+
   override def applySignal(signal: GearTilterState): Unit = {
-    if (collectorExtended.get) {
+    if (gearOpened.get) {
+      hardware.pneumatic.set(true)
+    } else if (collectorExtended.get) {
       hardware.pneumatic.set(false)
     } else {
       hardware.pneumatic.set(signal == GearTilterExtended)
