31749fc4c372c9498044f1faa3c471903d8b2c52
Nithya S <hi2nithya@gmail.com>
2017-10-01 15:34:50 -0700

    cleaned up code and added histogram

---
+++ src/main/scala/com/lynbrookrobotics/seventeen/drivetrain/Histogram.java
@@ -0,0 +1,61 @@
+package com.lynbrookrobotics.seventeen.drivetrain;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import edu.wpi.first.wpilibj.Timer;
+
+public class Histogram {
+
+    protected final long[] histogram;
+    public final double min, max;
+    public final double interval;
+    public final int bins;
+    Timer t; PrintWriter p;
+
+    public Histogram(final double min, final double max, final int bins) {
+        this.min = min;
+        this.max = max;
+        this.bins = bins;
+        interval = (max - min) / bins;
+        histogram = new long[bins + 2]; // bins and "less than min" and "greater than max"
+        t = new Timer();
+        try {
+            p = new PrintWriter(new File("/home/lvuser/timelog"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Record() {
+        double value = t.get();
+        if (value < min) histogram[0]++;
+        else if (value > max) histogram[bins + 1]++;
+        else histogram[1 + (int) ((value - min) / interval)]++;
+    }
+
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        sb.append(min);
+        sb.append(" : ");
+        sb.append(histogram[0]);
+        sb.append('\n');
+
+        for (int i = 1; i < bins + 1; i++) {
+            sb.append((float) (min + (interval * (i - 1))));
+            sb.append(" to ");
+            sb.append((float) (min + (interval * i)));
+            sb.append(" : ");
+            sb.append(histogram[i]);
+            sb.append('\n');
+        }
+
+        sb.append(">");
+        sb.append(max);
+        sb.append(" : ");
+        sb.append(histogram[bins+1]);
+        sb.append('\n');
+
+        return sb.toString();
+    }
+}
--- src/main/scala/com/lynbrookrobotics/seventeen/drivetrain/TimeStats.java
+++ src/main/scala/com/lynbrookrobotics/seventeen/drivetrain/TimeStats.java
@@ -1,22 +1,34 @@
 package com.lynbrookrobotics.seventeen.drivetrain;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import edu.wpi.first.wpilibj.Timer;
+
 
 public class TimeStats {
     double min, max, avg, sq;
     long count, logcount, initcount;
     double prev;
+    Timer t;
+    PrintWriter p;
 
     public TimeStats(int l, int i) {
         Reset();
         logcount = l; initcount = i; count = 0; prev = 0; avg = 0; sq = 0;
+        t = new Timer();
+        try {
+            p = new PrintWriter(new File("/home/lvuser/timelog"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void Reset () {
         min = 1E6; max = 0;
     }
 
-    public void Record (PrintWriter p, double timer) {
+    public void Record () {
+        double timer = t.get();
         double duration = timer - prev;
         if (count > initcount) {
             if (duration > max) max = duration;
