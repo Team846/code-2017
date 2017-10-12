c71162628a7e8fc31b640f182e84fd76f7d89087
nikwalia <nikash.walia@gmail.com>
2017-10-11 20:47:24 -0700

    testing

--- src/main/scala/com/lynbrookrobotics/seventeen/CoreRobot.scala
+++ src/main/scala/com/lynbrookrobotics/seventeen/CoreRobot.scala
@@ -300,7 +300,7 @@ class CoreRobot(configFileValue: Signal[String], updateConfigFile: String => Uni
   }
 
   val dashboard = Future {
-    val dashboard = new FunkyDashboard(8, 8080)
+    val dashboard = new FunkyDashboard(20, 8080)
     dashboard.start()
     dashboard
   }
@@ -330,24 +330,24 @@ class CoreRobot(configFileValue: Signal[String], updateConfigFile: String => Uni
     ))
 
     drivetrain.foreach { d =>
-      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.leftVelocity.map(_.toFeetPerSecond).toTimeSeriesNumeric("Left Ground Velocity"))
-      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.rightVelocity.map(_.toFeetPerSecond).toTimeSeriesNumeric("Right Ground Velocity"))
+//      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.leftVelocity.map(_.toFeetPerSecond).toTimeSeriesNumeric("Left Ground Velocity"))
+//      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.rightVelocity.map(_.toFeetPerSecond).toTimeSeriesNumeric("Right Ground Velocity"))
 
-      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.leftPosition.map(_.toFeet).toTimeSeriesNumeric("Left Ground"))
-      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.rightPosition.map(_.toFeet).toTimeSeriesNumeric("Right Ground"))
+      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.forwardPosition.map(_.toFeet).toTimeSeriesNumeric("forward"))
+//      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.rightPosition.map(_.toFeet).toTimeSeriesNumeric("Right Ground"))
 
-      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.rootDataStream
-        .map(d => (d.leftEncoderRotation * drivetrainProps.get.gearRatio).toDegrees).toTimeSeriesNumeric("Left Wheel Rotation"))
+//      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.rootDataStream
+//        .map(d => (d.leftEncoderRotation * drivetrainProps.get.gearRatio).toDegrees).toTimeSeriesNumeric("Left Wheel Rotation"))
+//
+//      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.rootDataStream
+//        .map(d => (d.rightEncoderRotation * drivetrainProps.get.gearRatio).toDegrees).toTimeSeriesNumeric("Right Wheel Rotation"))
 
-      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.rootDataStream
-        .map(d => (d.rightEncoderRotation * drivetrainProps.get.gearRatio).toDegrees).toTimeSeriesNumeric("Right Wheel Rotation"))
-
-      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.turnVelocity.map(_.toDegreesPerSecond).toTimeSeriesNumeric("Turn Velocity"))
+//      board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.turnVelocity.map(_.toDegreesPerSecond).toTimeSeriesNumeric("Turn Velocity"))
       board.datasetGroup("Drivetrain").addDataset(drivetrainHardware.turnPosition.map(_.toDegrees).toTimeSeriesNumeric("Rotational Position"))
       board.datasetGroup("Drivetrain").addDataset(simpsonsPosition.map(_.x.toFeet).toTimeSeriesNumeric("Simpsons x Position"))
       board.datasetGroup("Drivetrain").addDataset(simpsonsPosition.map(_.y.toFeet).toTimeSeriesNumeric("Simpsons y position"))
-      board.datasetGroup("Drivetrain").addDataset(regPosition.map(_.x.toFeet).toTimeSeriesNumeric("Simpsons x Position"))
-      board.datasetGroup("Drivetrain").addDataset(regPosition.map(_.y.toFeet).toTimeSeriesNumeric("Simpsons y position"))
+      board.datasetGroup("Drivetrain").addDataset(regPosition.map(_.x.toFeet).toTimeSeriesNumeric("Reg x Position"))
+      board.datasetGroup("Drivetrain").addDataset(regPosition.map(_.y.toFeet).toTimeSeriesNumeric("Reg y position"))
 
 
     }
